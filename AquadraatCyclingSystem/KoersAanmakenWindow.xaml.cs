ebb567519adc86d593a10fd89fac8eef5db8374a
r0452105 <61143931+r0452105@users.noreply.github.com>
2020-09-20 14:28:25 +0200

    routinesave

---
+++ AquadraatCyclingSystem/KoersAanmakenWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AquadraatCyclingSystem
+{
+    /// <summary>
+    /// Interaction logic for KoersAanmakenWindow.xaml
+    /// </summary>
+    public partial class KoersAanmakenWindow : Window
+    {
+        public KoersAanmakenWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRandomRonde_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
